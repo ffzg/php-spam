ccce7a7c8b2ca722843c23b43dd832612603041c
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-10-11 18:48:55 +0200

    fixed google-sites plugin exclusion

--- 3-grep.sh
+++ 3-grep.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 
 sudo find /srv/www/ -size +12k -name '*.php' -exec grep --with-filename 'if(!isset($GLOBALS\["' {} \; | \
-grep -v grep -v wp-content/plugins/google-sitemap-generator | \
+grep -v google-sitemap-generator/sitemap- | \
 tee $0-infected
 
 cat $0-infected | cut -d: -f1
